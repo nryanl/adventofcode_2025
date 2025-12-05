50c9116af134d39055d64d9eef353a42d8c9cf22
Nicholas Lindsey <nryanl@icloud.com>
2025-12-04 21:52:28 -0500

    Day 1 Part 2

--- solutions/day1.py
+++ solutions/day1.py
@@ -8,21 +8,39 @@ def day1():
 
     # Process each turn
     f = open("inputs/day1.txt")
+
     for line in f:
         # Parse the magnitude of the turn
         num = int(line[1:])
 
-        # Determine direction of dial turn
-        match line[0]:
-            case 'L':
-                dial_pos -= num
-            case 'R':
-                dial_pos += num
-        dial_pos %= 100
+        # Calculate old and new position
+        old_pos = dial_pos
+        new_pos_no_mod = dial_pos + num if line[0] == 'R' else dial_pos - num
+        print(f"{new_pos_no_mod % 100}", end="")
 
-        # Check our current position
-        if dial_pos == 0:
-            num_zeroes += 1
+        # Calculate number of wraps
+        # new_pos_no_mod >= 100                (wrap on first rotation)
+        # new_pos_no_mod <  0 and old_pos > 0  (wrap on first rotation)
+        # new_pos_no_mod <  0 and old_pos == 0 (no wrap on first rotation)
+        # new_pos_no_mod == 0                  (no wrap, we landed exactly on 0)
+        match [old_pos, new_pos_no_mod]:
+            case _ if new_pos_no_mod >= 100:
+                # WRAP RIGHT (from ZERO or POSITIVE)
+                # cross 0 at 100, 200, 300, ...
+                num_zeroes += new_pos_no_mod // 100
+            case _ if new_pos_no_mod < 0 and old_pos > 0:
+                # WRAP LEFT (from STRICTLY POSITIVE)
+                # cross at 0, then count additional wraps
+                num_zeroes += 1 + ((-new_pos_no_mod) // 100)
+            case _ if new_pos_no_mod < 0 and old_pos == 0:
+                # WRAP LEFT (from ZERO): only count additional wraps
+                num_zeroes += (-new_pos_no_mod) // 100
+            case _ if new_pos_no_mod == 0:
+                # If this case is hit, there's no wrap and we landed exactly on zero.
+                num_zeroes += 1
+
+        # Set new dial position
+        dial_pos = new_pos_no_mod % 100
 
     print(num_zeroes)
 
